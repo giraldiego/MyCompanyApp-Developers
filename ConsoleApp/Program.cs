116d3c17fe09a5190207d6017098f241b72f817f
giraldiego <giraldiego@gmail.com>
2021-09-23 16:09:14 -0500

    Refactor and rename Aplication Layer to ConsoleApp layer

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿﻿using System;
+using Domain;
+using Persistence;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Entity Framework!");
+        }
+    }
+}
