391b988cc26237a2c66ac969d5bf83c212def05b
giraldiego <giraldiego@gmail.com>
2021-09-23 15:54:02 -0500

    Refactor Persistence layer

---
+++ Persistence/AppRepos/AppDbContext.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+
+namespace Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Manager> Managers { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        // public DbSet<Company> Company { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=MyCompanyAppDB;user=SA;password=MinTic2021");
+        }
+
+    }
+}
---
+++ Persistence/install_ef.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+dotnet add package Microsoft.EntityFrameworkCore
+dotnet add package Microsoft.EntityFrameworkCore.Tools
+dotnet add package Microsoft.EntityFrameworkCore.Design
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer
