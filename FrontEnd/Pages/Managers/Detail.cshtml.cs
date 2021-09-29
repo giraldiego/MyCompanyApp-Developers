858bbd9047a1a09ff9cd7aba51561b7aebc6819e
giraldiego <giraldiego@gmail.com>
2021-09-29 09:08:10 -0500

    Add list and detail(broken) page for Managers

---
+++ FrontEnd/Pages/Managers/Detail.cshtml.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Persistence;
+using Domain;
+
+namespace FrontEnd.Pages.Managers
+{
+    public class DetailModel : PageModel
+    {
+        private readonly IRepoPerson repoEntity;
+        public Manager Entity { get; set; }  // Change entity type to match class
+
+        public DetailModel()
+        {
+            this.repoEntity = new RepoManager(new AppDbContext());  // Repo type to match class
+        }
+
+        public IActionResult OnGet(int pk)
+        {
+            Entity = (Manager)repoEntity.Detail(pk);
+            if(Entity == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            else
+            {
+                return Page();
+            }
+
+        }
+    }
+}
